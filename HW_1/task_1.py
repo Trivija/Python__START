fba0018efefba0bf4092af0e915f025a4af25a9f
Anna <110714010+Trivija@users.noreply.github.com>
2022-12-06 19:40:02 +0300

    Update task_1.py

--- HW_1/task_1.py
+++ HW_1/task_1.py
@@ -11,6 +11,7 @@ True
 1
 False
 """
+"""
 number = int(input("Введите число дня недели от 1 до 7: "))
 
 if number < 1 or number > 7:
@@ -19,3 +20,26 @@ elif number > 5:
     print('Да, этот день выходной!')
 else:
     print('Увы, но это рабочий день!')
+    """
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("True")
+    elif 0 < num < 6:
+        print("False")
+    else:
+        print("число вне пределов 7 дней")
+
+
+num = InputNumbers("Введите число: ")
+checkNumber(num)
